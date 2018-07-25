fe5c5a52305a64aa26e5ba1e4a53085ac3fa6f80
Dasith Wijesiriwardena <dasiths@hotmail.com>
2018-07-25 23:05:19 +1000

    Add more testing

---
+++ NimbleConfig.Core.Tests/Settings/SomeEnum.cs
@@ -0,0 +1,9 @@
+﻿namespace NimbleConfig.Core.Tests.Settings
+{
+    public enum SomeEnum
+    {
+        One = 1,
+        Two = 2,
+        Three = 3
+    }
+}
---
+++ NimbleConfig.Core.Tests/Setup/ConfigurationFactoryCreator.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+using Microsoft.Extensions.Configuration;
+using NimbleConfig.Core.Factory;
+using NimbleConfig.Core.Options;
+using NimbleConfig.Core.Resolvers;
+
+namespace NimbleConfig.Core.Tests.Setup
+{
+    public class ConfigurationFactoryCreator
+    {
+        public static ConfigurationFactory Create()
+        {
+            // Construct the IConfiguration
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            var configuration = (IConfiguration)builder.Build();
+
+            // Configure the core pieces
+            var keynameResolver = new KeyNameResolver();
+            var parserResolver = new ParserResolver();
+            var configResolver = new ConfigurationReaderResolver();
+            var constructorResolver = new ValueConstructorResolver();
+
+            var options = new ConfigurationOptions();
+
+            var configuratinFactory = new ConfigurationFactory(configuration,
+                options,
+                keynameResolver,
+                configResolver,
+                parserResolver,
+                constructorResolver);
+            return configuratinFactory;
+        }
+    }
+}
