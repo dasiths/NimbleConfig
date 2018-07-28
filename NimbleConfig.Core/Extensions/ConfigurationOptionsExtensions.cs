223903ba53bc90d6c498dbf91ef8ca7b488f117f
Dasith Wijesiriwardena <dasiths@hotmail.com>
2018-07-28 23:10:34 +1000

    more extensible options and setting scanning

---
+++ NimbleConfig.Core/Extensions/ConfigurationOptionsExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using NimbleConfig.Core.Configuration;
+using NimbleConfig.Core.Options;
+
+namespace NimbleConfig.Core.Extensions
+{
+    public static class ConfigurationOptionsExtensions
+    {
+        public static void SetGlobalPrefix(this IConfigurationOptions configurationOptions, string prefix, Type[] onlyForTheseTypes = null, Type[] exceptTheseTypes = null)
+        {
+            configurationOptions.CustomKeyName = (type, name) =>
+            {
+                if (onlyForTheseTypes != null && !onlyForTheseTypes.Contains(type))
+                {
+                    return name;
+                }
+
+                if (exceptTheseTypes != null && exceptTheseTypes.Contains(type))
+                {
+                    return name;
+                }
+
+                return new KeyName(prefix, name.QualifiedKeyName);
+            };
+        }
+    }
+}
